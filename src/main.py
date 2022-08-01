c811c60a801ee94e818ade4f99d01c74c04d42f9
Glenn WS <sondellglenn@gmail.com>
2022-08-01 22:12:04 +0200

    classes refactor init

--- src/main.py
+++ src/main.py
@@ -3,34 +3,6 @@ from clear import clear
 from time import sleep
 
 
-   
-
-secret = "tulpan".upper()    
-
-word = "".join("_" for _ in secret)
-
-counter = 0
-
-
-def ask():
-
-    print("The word: ", word)
-    print("Enter a missing char!")
-    char = input().upper()
-
-    if len(char)<1:
-        print("Try again")
-        char = ask()
-    return char
-    
-
-def escalate(counter) -> int:    
-    if char not in secret:
-        clear()
-        print(PICS[counter])   
-        counter +=1
-    return counter        
-        
 def replacer(s, newstring, index):
     if index < 0:  # add it to the beginning
         return newstring + s
@@ -40,24 +12,64 @@ def replacer(s, newstring, index):
     # insert the new string between "slices" of the original
     return s[:index] + newstring + s[index + 1:]
 
+    
+class Game():
 
-clear()
-print("Welcome to hang!")
-while counter < 7:
-    char = ask()
+    def __init__(self, secret: str):
+        self.secret = secret.upper()
+        self.word = "".join("_" for _ in secret)
+        self.used = ""
+        self.mistakes = 0
     
-    if char in secret:
-        for i, c in enumerate(secret):
-            if c == char:
-                word =  replacer(word, c, i)
+    def greet(self):
+        clear()
+        print("Hello Ladies!")
+        print("Welcome to hang!")
+        print("Guess the word or be hung!")
+
+    def _ask(self) -> str:
+        print("The word: ", self.word)
+        if len(self.used) > 0:
+            print("Mistakes: ", [c for c in self.used])
+
+        print("Enter a missing char!")
+        char = input().upper()
+
+        if len(char)<1:
+            print("Try again")
+            char = ask()
+        return char
+    
+
+    def _check(self, char: str):    
+        print(char, self.secret)
+        if char not in self.secret:
+            clear()
+            print(PICS[self.mistakes])   
+            self.mistakes +=1
+            self.used += char
+            return False
+        return True
+            
+    def play(self):
+        self.greet()
+        while self.mistakes < 7:
+            char = self._ask()
+    
+            if self._check(char):
+                for i, c in enumerate(self.secret):
+                    if c == char:
+                        self.word =  replacer(self.word, c, i)
                 
-        if word==secret:
-            print("win")
-            sleep(5)
-            break
-    
-    else:
-       counter =  escalate(counter)
+                if self.word==self.secret:
+                    print(self.secret)
+                    print("You won!")
+                    sleep(5)
+                    break
         
+        print("Game over!")
         
-print("Game over!")
+
+game = Game("läggdags")
+
+game.play()
