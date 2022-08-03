e80c2c5ab83399639c346467ee26fa3ce4d0001f
Glenn WS <sondellglenn@gmail.com>
2022-08-03 21:32:49 +0200

    multi_input

--- src/main.py
+++ src/main.py
@@ -11,49 +11,48 @@ class Game():
         self.mistakes = 0
     
     def greet(self):
-        clear()
         print("Welcome to Hang")
         print("Guess the word or be hung!")
 
-    def _ask(self) -> str:
-        if len(self.used) > 0:
+    def draw(self):
+        clear()
+        self.greet()
+        
+        if self.mistakes > 0:
             print(PICS[self.mistakes])   
-            print("Mistakes: ", [c for c in self.used])
-
+            print("Mistakes: ", ", ".join(c for c in self.used))
         print("Enter a missing char!")
         print("The word: ", self.word)
-
-        char = input().upper()
-
-        if len(char)<1:
+    
+    def ask(self) -> str:
+        guess = input().upper()
+        if len(guess)<1:
             print("Try again")
-            char = self._ask()
-        return char
+            guess = self.ask()
+        return guess
     
 
-    def _check(self, char: str):    
-        if char not in self.secret:
-            self.mistakes +=1
-            self.used += char
-            return False
-        return True
+    def check(self, guess: str):    
+        for char in guess:
+            if char in self.secret:
+                for i, c in enumerate(self.secret):
+                    if c == char:
+                        self.word = replacer(self.word, c, i)
+            else:
+                self.mistakes += 1
+                self.used += char
             
     def play(self):
         while self.mistakes < 7:
-            clear()
-            self.greet()
-            char = self._ask()
-            if self._check(char):
-                for i, c in enumerate(self.secret):
-                    if c == char:
-                        self.word =  replacer(self.word, c, i)
-                
-                if self.word==self.secret:
-                    clear()
-                    print(self.secret)
-                    print("You won!")
-                    sleep(5)
-                    break
+            self.draw()
+            guess = self.ask()
+            self.check(guess)
+            if self.word==self.secret:
+                self.draw()
+                print(self.secret)
+                print("You won!")
+                sleep(5)
+                break
         
         print("Game over!")
         
